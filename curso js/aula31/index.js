1b0b43de8c48db926d1e954d98a952ae62e9e3ab
guiayres854500-ctrl <guiayres854500@gmail.com>
2025-11-03 15:43:14 -0300

    Add files via upload

---
+++ curso js/aula31/index.js
@@ -0,0 +1,37 @@
+const verdadeira = true;
+
+//Let tem escopo de bloco { . . . bloco }
+//Var só tem escopo de função
+
+//let nome = 'Luiz'; // criando
+//var nome2 = 'Luiz'; // criando
+//
+//
+//if (verdadeira) {
+//    let nome = 'Otávio'; //criando
+//    var nome2 = 'Rogério'; //redeclarando
+//
+//    if (verdadeira) {
+//        var nome2 = 'Ronaldo'; //Redeclarada
+//        let nome = 'outro';
+//    }
+//} 
+//
+//console.log(nome, nome2);
+
+
+//function falaOi () {
+//    
+//    if (verdadeira) {
+//        let nome = 'Luiz';
+//        var sobreNome = 'Miranda';
+//    }
+//    
+//    console.log(sobreNome);
+//}
+//
+//falaOi();
+
+
+let sobrenome = 'Miranda';
+console.log(sobrenome);
